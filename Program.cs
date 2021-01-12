d50ff70121fab660c142b32daee54eeeaa397293
unknown <pedrofelipbarros@gmail.com>
2021-01-12 12:43:17 -0300

    exercicio5 finalizado

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inicio;
+            int resultado;
+
+            Console.WriteLine("Gostaria de saber a tabuada do 1 ao 10? digite sim ou não");
+            inicio = Console.ReadLine();
+            
+            if (inicio == "sim")
+            {
+                for (var i = 0; i <= 10; i++)
+                {
+                        Console.WriteLine("---------------------------");
+                        Console.WriteLine("tabuada do "+(i));
+                    for (var n = 0; n <= 10; n++)
+                    {
+                        resultado = i*n;
+                        Console.WriteLine($"{n} x {i} = {resultado} ");
+                       
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Obrigado pela visita :)");
+            }
+        }
+    }
+}
